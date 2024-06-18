3046b186b3f53d12759c1bcfae352c1e87a03ec1
Salma Shaik <salmaiiitn@gmail.com>
2024-06-18 21:04:39 +0530

    WIP Narration

--- Assets/Scripts/NarrativeController.cs
+++ Assets/Scripts/NarrativeController.cs
@@ -5,9 +5,13 @@ public class NarrativeController : MonoBehaviour
 {
     public TextMeshProUGUI narrativeText;
     private string[] storyLines = {
-        "Meet Ruby, the Ruby-throated Hummingbird...",
+        "Meet Ruby\nRuby-throated Hummingbird",
         "Ready for her first migration from North America to Central America....",
-        "Facing many challenges and foes..."
+        "With an incredible 3,000km to travel in just under three weeks, Ruby's migration poses countless dangers.",
+        "A tempestuous sky can prove fatal for the migrants.",
+        "Even heavy rain alone could spell disaster for such a delicate species.",
+        "A new land means new predators, and hurdles.",
+        "A new journey soon awaits Ruby!"
     };
     private int currentLineIndex = 0;
 
@@ -33,7 +37,7 @@ public class NarrativeController : MonoBehaviour
         }
         else
         {
-            narrativeText.text = "The End.";
+            narrativeText.text = "Press Enter to Start";
         }
     }
 }
